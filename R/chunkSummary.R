c1a2a8a322e49cffeaa856bb8371e56be0623a0e
YuweiNi45 <yuweini45@gmail.com>
2018-05-27 03:10:20 -0400

    Add new function: chunkSummary

---
+++ R/chunkSummary.R
@@ -0,0 +1,12 @@
+chunkSummary<- function(data){
+  n<-nrow(data)
+  n1<-ceiling(n/4)
+  n2<-ceiling(n/2)
+  n3<-ceiling(3*n/4)
+  n4<-n
+  dat1<-data[1:n1,,]
+  dat2<-data[(n1+1):n2,,]
+  dat3<-data[(n2+1):n3,,]
+  dat4<-data[(n3+1):n4,,]
+  dat1
+}
