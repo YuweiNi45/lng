f94573b01003f3b6eaeab24ede1952666c572aa7
YuweiNi45 <yuweini45@gmail.com>
2018-05-29 23:44:27 -0400

    Delete the useless parts

--- src/timesTwo.cpp
+++ src/timesTwo.cpp
@@ -1,15 +1,10 @@
+
 #include <Rcpp.h>
 using namespace Rcpp;
-
-// This is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using the Rcpp::sourceCpp
-// function (or via the Source button on the editor toolbar). Learn
-// more about Rcpp at:
-//
-//   http://www.rcpp.org/
-//   http://adv-r.had.co.nz/Rcpp.html
-//   http://gallery.rcpp.org/
-//
+//' Multiple a number
+//'
+//' @param x A single number
+//' @export
 
 // [[Rcpp::export]]
 NumericVector timesTwo(NumericVector x) {
